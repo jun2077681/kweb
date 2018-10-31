352be4d3a76aecd9a37d623fc6acd5613610b2ff
hasu <jun2077681@naver.com>
2018-10-31 18:04:43 +0900

    assignmnet_promise edited.

--- week2/assignment_promise.js
+++ week2/assignment_promise.js
@@ -1,23 +1,49 @@
-const fs = require('fs').promises;
+const fs = require('fs');
 
-fs.writeFile('./1.txt', '11111')
+function writeFile(name, data) {
+	return new Promise((resolve, reject) => {
+		fs.writeFile(name, data, (error, data) => {
+			if(error) {
+				reject(error);
+			} else {
+				resolve(data);
+			}
+		});
+	});
+}
+
+function readFile(name, type) {
+	return new Promise((resolve, reject) => {
+		fs.readFile(name, type, (error, data) => {
+			if(error) {
+				reject(error);
+			} else {
+				resolve(data);
+			}
+		});
+	});
+}
+
+
+
+writeFile('./1.txt', '11111')
 	.then(() => {
 		console.log('1.txt created');
-		return fs.readFile('./1.txt','UTF-8');
+		return readFile('./1.txt','UTF-8');
 	})
 	.then((data1) => {
-		return fs.writeFile('./2.txt', data1+'22222');
+		return writeFile('./2.txt', data1+'22222');
 	})
 	.then(() => {
 		console.log('2.txt created');
-		return fs.readFile('./2.txt','UTF-8');
+		return readFile('./2.txt','UTF-8');
 	})
 	.then((data2) => {
-		return fs.writeFile('./3.txt',data2 + '33333');
+		return writeFile('./3.txt',data2 + '33333');
 	})
 	.then(() => {
 		console.log('3.txt created');
-		return fs.readFile('./3.txt','UTF-8');
+		return readFile('./3.txt','UTF-8');
 	})
 	.then((data3) => {
 		console.log(data3);	
